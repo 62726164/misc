d252dce27fb5b70f987a6aaf7d14c92a0fab9026
62726164 <btilley@gatech.edu>
2022-03-29 22:43:01 +0000

    landlock-example

---
+++ go/landlock/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/landlock-lsm/go-landlock/landlock"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// simple program that demonstrates how landlock works in Go on Linux systems.
+func main() {
+	var help = flag.Bool("help", false, "landlock-example -f /path/to/file.txt")
+	var file = flag.String("f", "", "the file path to read")
+
+	flag.Parse()
+	if *help || len(os.Args) == 1 {
+		flag.PrintDefaults()
+		return
+	}
+
+	// allow the program to read files in /home/user/tmp
+	err := landlock.V1.RestrictPaths(landlock.RODirs("/home/user/tmp"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// attempt to read a file
+	bytes, err := ioutil.ReadFile(*file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(bytes))
+}
