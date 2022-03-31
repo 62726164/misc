1d7dbcb94e88e3b0549872d14349db291b6dec53
62726164 <btilley@gatech.edu>
2022-03-31 10:05:59 -0400

    fix-git.sh

---
+++ scripts/fix-git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config pull.rebase false
+git config --local user.name 62726164
+git config --local user.email btilley@gatech.edu
+git config commit.gpgsign true
+git config user.signingkey 9C2CC588967D53A3
+
