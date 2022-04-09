4e9c24d6cca801c245c107581f24090621694111
62726164 <btilley@gatech.edu>
2022-04-08 20:36:42 -0400

    loop over users

--- age/gen-age-keys/main.go
+++ age/gen-age-keys/main.go
@@ -8,7 +8,7 @@ import (
 	"filippo.io/age"
 )
 
-func main() {
+func genKey(user string) {
 	// This is equivalent to age-keygen
 	identity, err := age.GenerateX25519Identity()
 	if err != nil {
@@ -18,4 +18,12 @@ func main() {
 	fmt.Printf("# created: %s\n", time.Now().Format(time.RFC3339))
 	fmt.Printf("# public key: %s\n", identity.Recipient().String())
 	fmt.Printf("%s\n", identity.String())
+}
+
+func main() {
+	users := []string{"one", "two", "three"}
+
+	for _, user := range users {
+		genKey(user)
+	}
 }
