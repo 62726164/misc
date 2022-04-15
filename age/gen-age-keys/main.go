e62cbc7fbae2ec303cb81be7f34c22cdcc9a9a1b
62726164 <btilley@gatech.edu>
2022-04-14 20:27:03 -0400

    encrypt with password

--- age/gen-age-keys/main.go
+++ age/gen-age-keys/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"time"
 
 	"filippo.io/age"
 )
 
+// genKey - This is equivalent to age-keygen from the cli
 func genKey(user string) {
-	// This is equivalent to age-keygen
 	identity, err := age.GenerateX25519Identity()
 	if err != nil {
 		log.Fatalf("Failed to generate key pair: %v", err)
@@ -20,10 +22,39 @@ func genKey(user string) {
 	fmt.Printf("%s\n", identity.String())
 }
 
+// pwEncrypt - Encrypt with a password.
+func pwEncrypt(password string) {
+	nsr, err := age.NewScryptRecipient(password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create("enc.age")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	w, err := age.Encrypt(f, nsr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := io.WriteString(w, "The plain text.\n"); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 	users := []string{"one", "two", "three"}
 
 	for _, user := range users {
 		genKey(user)
 	}
+
+	pwEncrypt("howdy there partner")
 }
