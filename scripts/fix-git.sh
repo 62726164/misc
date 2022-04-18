8f884d98819a2dfcc4338772fc8e18a28c4e8b08
62726164 <btilley@gatech.edu>
2022-04-17 20:53:42 -0400

    add qubes-gpg-client-wrapper to fix-git

--- scripts/fix-git.sh
+++ scripts/fix-git.sh
@@ -3,6 +3,6 @@
 git config pull.rebase false
 git config --local user.name 62726164
 git config --local user.email btilley@gatech.edu
+git config --local gpg.program qubes-gpg-client-wrapper
 git config commit.gpgsign true
 git config user.signingkey 9C2CC588967D53A3
-
